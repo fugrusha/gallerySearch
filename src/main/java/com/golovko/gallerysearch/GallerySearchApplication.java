f8daf2024dd28317744c296d57a52062603a7e3e
fugrusha <andrew.golowko@gmail.com>
2020-07-18 11:19:16 +0300

    initial commit

---
+++ src/main/java/com/golovko/gallerysearch/GallerySearchApplication.java
@@ -0,0 +1,13 @@
+package com.golovko.gallerysearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GallerySearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GallerySearchApplication.class, args);
+	}
+
+}
